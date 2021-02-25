2aef658f4e252fc578b59829094707413a283ed3
is714150 <77624602+is714150@users.noreply.github.com>
2021-02-24 22:48:53 -0600

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,15 @@
 let gulp = require('gulp');
 let sass = require('gulp-sass');
-let typescript = require('gulp-typescript');
-
-gulp.task('styles', function(){
-    return gulp.src('src/**/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('final/styles'));
-})
-
-gulp.task('scripts', function(){
-    let tsConfig = typescript.createProject('tsconfig.json');
-    return tsConfig.src()
-    .pipe(tsConfig())
-    .pipe(gulp.dest('final/scripts'));
-})
 
 gulp.task('html', function(){
     return gulp.src('src/index.html')
-    .pipe(gulp.dest('final'));
+        .pipe(gulp.dest('dist'))
+        
+
+})
+gulp.task('styles', function(){
+    return gulp.src('src/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('dist/assets/styles'))
 })
 
-gulp.task('default', gulp.parallel(['styles', 'scripts', 'html']));
